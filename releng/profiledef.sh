c3c7de1d509cb759548dd6a897d48e97add1c492
MS-DOS Manfred <hoefer9@gmail.com>
2024-01-20 13:19:42 +0100

    Updating ISO

--- releng/profiledef.sh
+++ releng/profiledef.sh
@@ -10,9 +10,8 @@ install_dir="arch"
 buildmodes=('iso')
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
            'uefi-ia32.grub.esp' 'uefi-x64.grub.esp'
-           'uefi-ia32.grub.eltorito' 'uefi-x64.grub.eltorito'
-           'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
-arsch="x86_64"
+           'uefi-ia32.grub.eltorito' 'uefi-x64.grub.eltorito')
+arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
 #airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
